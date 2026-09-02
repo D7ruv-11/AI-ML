4b251105a65046daff242abe0c5da5ff11e681b7
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2026-09-03 00:56:35 +0530

    Add Assignment-1/ass1-Q6.py

---
+++ Assignment-1/ass1-Q6.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number a:"))
+b  = int(input("Enter the value of nmber b: "))
+c = a+b
+b = c-b 
+a = c-a
+print("After swaping the number a become:",a)
+print("After swaping the number b become:", b)
